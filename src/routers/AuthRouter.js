d49bef7e2cffa5f07b012925be148f18b41720ea
Miwell <miguelinformatica01@gmail.com>
2021-01-08 11:46:31 +0100

    feat: Routers working

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch,Route, Redirect } from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { RegisterScreen } from '../components/auth/RegisterScreen'
+
+export const AuthRouter = () => {
+    return (
+        <div>
+            <Switch>
+                <Route path="/auth/login" exact component={LoginScreen}/>
+                <Route path="/auth/register" exact component={RegisterScreen}/>
+                <Redirect to="/auth/login"/>
+            </Switch>
+        </div>
+    )
+}
