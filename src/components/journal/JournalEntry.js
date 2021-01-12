c0d4af2d107ec3cd294b38cb407a791625d6a5e8
Miwell <miguelinformatica01@gmail.com>
2021-01-12 12:33:20 +0100

    feat: static  journal entries added

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+            <div className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80)'
+                }}>
+
+            </div>
+            <div className="journal__entry-body">
+                <p className="journal__entry-title"> New day</p>
+                <p className="journal__entry-content">
+                    Duis minim nisi minim eiusmod irure.   velit Lorem exercitation.
+                </p>
+            </div>
+            <div className="journal__entry-date">
+                <span>Monday</span>
+                <h4>28</h4>
+
+            </div>
+
+        </div>
+    )
+}
