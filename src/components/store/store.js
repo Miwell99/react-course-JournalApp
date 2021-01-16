15ed7189f4baffbc71edc9d0b116729bd2a55fca
Miwell <miguelinformatica01@gmail.com>
2021-01-16 12:02:18 +0100

    feat:reducer generated and register on redux store

---
+++ src/components/store/store.js
@@ -0,0 +1,10 @@
+import {createStore, combineReducers} from 'redux';
+import { authReducer } from '../../reducers/authreducer';
+
+const reducers = combineReducers({
+
+    // Reducers List
+    auth: authReducer,
+});
+
+export const store = createStore(reducers);
