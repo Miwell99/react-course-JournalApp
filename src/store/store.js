04af090ecafe239194a7b7c6f70c8143b090d6df
Miwell <miguelinformatica01@gmail.com>
2021-01-18 10:11:40 +0100

    fix: authreducer renamed to authreducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import { authReducer } from '../reducers/authreducer';
+import { authReducer } from '../reducers/authReducer';
 
 // Middlewares devtools
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
