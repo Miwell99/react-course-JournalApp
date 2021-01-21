385216109356e91253d9281f0c1148ce573effed
Miwell <miguelinformatica01@gmail.com>
2021-01-21 11:07:20 +0100

    feat: ui loading sync actions generated

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,10 +1,20 @@
 import { types } from "../components/types/types"
 
+/* Note: Sync actions doesn't need return*/
+
 export const setError = (err) => ({
     type: types.uiSetError,
     payload: err,
 });
 
-export const removeError = ()=> ({
+export const removeError = () => ({
     type: types.uiRemoveError,
 });
+
+export const startLoading = () =>({
+    type: types.startLoading,
+})
+
+export const finishLoading = () =>({
+    type: types.finishLoading,
+})
